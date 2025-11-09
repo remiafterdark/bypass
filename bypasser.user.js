a1ec3094c3f30eafee05d487eedc0b1df00fbbf2
remiafterdark <155065631+remiafterdark@users.noreply.github.com>
2025-11-08 20:08:38 -0600

    Add user script for bypassing Volcano and Work.ink

---
+++ bypasser.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         volcano+work.ink bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Bypass Volcano and Work.ink links
+// @author       remi <3
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
+// @downloadURL  https://github.com/remiafterdark/bypass/raw/main/bypass.user.js
+// @updateURL    https://github.com/remiafterdark/bypass/raw/main/bypasser.user.js
+// @require      https://github.com/remiafterdark/bypass/raw/main/bypasser.js
+// ==/UserScript==
